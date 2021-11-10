588a8a5b3ab8585baaae5f960c3f87e52db20f95
Elale1984 <69740507+Elale1984@users.noreply.github.com>
2021-11-09 21:33:27 -0600

    Created an Adapter and ViewHolder for the calender events recyclerview

---
+++ app/src/main/java/us/libertybaptist/libertybaptistchurchapp/ViewHolders/rvCalViewHolder.java
@@ -0,0 +1,12 @@
+package us.libertybaptist.libertybaptistchurchapp.ViewHolders;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class rvCalViewHolder extends RecyclerView.ViewHolder {
+    public rvCalViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
